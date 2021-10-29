84dcfba13c48987590e752a9ff42e21bba206bd8
ismatullayevs <70445308+ismatullayevs@users.noreply.github.com>
2021-10-29 18:56:45 +0500

    First commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request, flash
+
+
+app = Flask(__name__)
+app.secret_key = "youwillneverfindthispassword"
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        flash("Your request has been sent", "info")
+    return render_template("test.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
