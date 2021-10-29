e72b60d88d1c74fcb5c5e2ae19c79ae0a5db9af9
ismatullayevs <70445308+ismatullayevs@users.noreply.github.com>
2021-10-29 21:35:15 +0500

    Fixed HEAD issue

--- app.py
+++ app.py
@@ -10,10 +10,7 @@ def index():
     if request.method == 'POST':
         flash("Your request has been sent", "info")
     return render_template("test.html")
-<<<<<<< HEAD
-=======
 
 
 if __name__ == "__main__":
     app.run()
->>>>>>> 75631fff5c85bf00dc5e1feadc0f3e4d97c2e62a
